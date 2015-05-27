c7acc2998289cf0f800d2693f7bdab34adc32f9f
arkhamrejek <paulbrunache@gmail.com>
2015-05-27 06:03:57 +0000

    updated user permissions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:email, :password)
+      params.require(:user).permit(:email, :password,:admin)
     end
 
     def correct_user
