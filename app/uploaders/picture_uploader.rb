8b92502964f65f039f8d2ee44cae2cd71b12ddd0
arkhamrejek <paulbrunache@gmail.com>
2015-05-27 08:08:39 +0000

    configuring dropbox for heroku

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -9,17 +9,18 @@ class PictureUploader < CarrierWave::Uploader::Base
   process resize_to_limit: [700,700]
 
   # Choose what kind of storage to use for this uploader:
-  
-  storage :dropbox
+  if Rails.env.production?
+    storage :dropbox
+  else 
+    storage :file 
+  end
 
-  
-
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/"
   end
-
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
