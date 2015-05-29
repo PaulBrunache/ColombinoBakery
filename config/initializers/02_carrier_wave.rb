68608cbb247bdc716b0bb50879b92dc9d27913e2
arkhamrejek <paulbrunache@gmail.com>
2015-05-29 07:18:10 +0000

    configuring cloudinary for heroku

--- config/initializers/02_carrier_wave.rb
+++ config/initializers/02_carrier_wave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   # config.app_key = ENV["APP_KEY"]
   # config.dropbox_app_secret = ENV["APP_SECRET"]
-  config.cloud_name = ENV["cloud_name"]
-  config.api_key = ENV["api_key"]
-  config.api_secret = ENV["api_secret"]
+  # config.cloud_name = ENV["cloud_name"]
+  # config.api_key = ENV["api_key"]
+  # config.api_secret = ENV["api_secret"]
   
   config.cache_dir = "#{Rails.root}/tmp/uploads"
 end
